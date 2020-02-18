fdae5c9b574939f0ea519dc27495b1cedf8acb0d
GhouseJohn <md.gosoddin@gmail.com>
2020-02-18 18:52:14 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,17 +9,12 @@ import{MaterialModule} from './material-module'
 import{FoodComponent} from './FoodComponent'
 import{HomeComponent} from './Home/HomeComponent'
 import{PageNotfoundComponent} from './Home/PageNotFoundComponent'
-
 import{ListofFoodComponent} from './FoodMenu/ListofFoodComponent'
 import{AppRoutingModule} from './AppRoutingModule '
-
 import { RouterModule, Routes } from '@angular/router';
-  import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
 
 //import{CompoenentRegModule} from './ComponentRegModule'
-
-
-
 
 const appRoutes: Routes = [
   { path: 'crisis-center', component: FoodComponent },
